54058dd5d27fea57be2107bc0f1279ab9e1457e2
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-13 17:47:14 -0800

    integers

--- Java-Basics-1/java-basics-1.md
+++
@@ -1,3 +0,0 @@
-[Alex's Tutorial](https://www.youtube.com/watch?v=az6SehZyY7U&list=PL59LTecnGM1NRUyune3SxzZlYpZezK-oQ)
-
-
---
+++ integers.java
@@ -0,0 +1,12 @@
+class Integers {
+    public static void main(String[] args){
+        // String a = "Alex Lee";
+        int a = 5;
+        int b = 2;
+        int c = 0;
+        int d = 9999999;
+        int e = -700;
+
+        System.out.print(a + b);
+    }
+}
