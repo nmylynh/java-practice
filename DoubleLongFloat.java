710e0f3aff3fc6514cca4b2bfbc27c224f01a4ef
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-13 17:56:28 -0800

    int, doubles, long, floats exercise

---
+++ DoubleLongFloat.java
@@ -0,0 +1,18 @@
+class DoubleLongFloat {
+    public static void main(String[] args){
+        // integers can only be whole numbers, you cannot have a decimal
+        // there's also a max size an int can be, if it's really long its called a long integer
+        int a = 9;
+        // long int
+        long b = 99999999999999L;
+        // decimals
+        float c = 2.5F;
+        // double is a really long float
+        double d = 5.4;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
