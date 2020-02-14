59a2bc2a93fc518b65a27a2b514e72155cc3bac0
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-13 16:22:47 -0800

    strings in java

--- QuickStart.java
+++ QuickStart.java
@@ -1,5 +1,6 @@
 class QuickStart {
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
+        System.out.println("Hello, World."); // differs from print in that print doesn't go to the next line
     }
 }
+
---
+++ StringsProgram.java
@@ -0,0 +1,16 @@
+class StringsProgram{
+    public static void main(String[] args){
+        System.out.println("Madlibs!");
+
+        String username = "magicPony420!";
+
+        System.out.println("My _____ sister accidentally ___ed all over her new ____");
+
+        String adjective = "flamboyant";
+        String verb = "fart";
+        String noun = "pogo stick";
+
+        System.out.println("My " + adjective + " sister accidentally " + verb + "ed all over her new " + noun + ".");
+
+    }
+}
