a2d26966662dc6dbc3dd458c6dfb587db2aa4ebd
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-16 15:02:26 -0800

    Create ROFLCOPTER.java

---
+++ ROFLCOPTER.java
@@ -0,0 +1,51 @@
+class ROFLCOPTER {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("         :LOL:ROFL:ROFL                       ");
+            System.out.println("           A            ");
+            System.out.println("        /---------                 ");
+            System.out.println(" LOL====        [] \\              ");
+            System.out.println("  L    \\            \\      ");
+            System.out.println("        \\____________]              ");
+            System.out.println("             I    I      ");
+            System.out.println("          ------------/             ");
+            System.out.println("                       ");
+            System.out.println("  Mitsy's ROFLCOPTER!                     ");
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            Thread.sleep(100);
+            System.out.println("ROLF:ROFL:LOL:                       ");
+            System.out.println("           A            ");
+            System.out.println("  L     /---------                 ");
+            System.out.println(" LOL====        [] \\              ");
+            System.out.println("       \\            \\      ");
+            System.out.println("        \\____________]              ");
+            System.out.println("             I    I      ");
+            System.out.println("          ------------/             ");
+            System.out.println("                       ");
+            System.out.println("  Mitsy's ROFLCOPTER!                     ");
+        }
+    }
+}
