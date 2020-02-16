80fca91356aa3bb20ca9c7ec8bf44a231c7dd1bf
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-16 13:51:46 -0800

    Create exponents.java

---
+++ exponents.java
@@ -0,0 +1,6 @@
+class Exponents{
+    public static void main(String[] args){
+        // what int does is to change the exponent to an integer
+        System.out.println((int) Math.pow(8.2, 5.1));
+    }
+}
