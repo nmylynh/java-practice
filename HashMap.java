30929c52eb52a01d6e63793436c7afe1f8d1d50e
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-17 22:09:11 -0800

    Create HashMap.java

---
+++ HashMap.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+class HashMapAwesomeness {
+    public static void main(String[] args){
+        // int a = 10;
+        // int b = 3;
+        // int c = 88;
+
+        // HashMap<String, Integer> happy = new HashMap<String, Integer>();
+        // happy.put("a", 10);
+        // happy.put("b", 3);
+        // happy.put("c", 88);
+
+        // System.out.println(happy.get("c"));
+
+        HashMap<String, String> fun = new HashMap<String, String>();
+        fun.put("bobbyJoe1996", "FluffyP0nies!");
+        fun.put("hellKittyFan21", "AloeVera?");
+        fun.put("coolguyswag", "password123");
+
+        fun.remove("helloKittyFan21");
+
+        // get keys and values:
+        System.out.println(fun.containsKey("coolguyswag"));
+        // outputs true
+
+        System.out.println(fun.containsValue("password123"));
+        // outputs true
+    }
+}
